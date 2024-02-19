b757ab894473b880d3a1bfc62f9db0ae9ebb8fcc
Leo Brucker <ct703@fzi.de>
2024-02-19 11:12:27 +0100

    add bit of realm logic

--- game.py
+++ game.py
@@ -6,3 +6,8 @@ class Game():
         self.map = Map(5, 5)
         self.realm = Realm(self.map)
         self.realm.found_city((2,2))
+    
+    def tick(self):
+        self.map.tick()
+
+    
--- realm.py
+++ realm.py
@@ -1,13 +1,14 @@
 from city import City
-
+from resources import Resource
 class Realm:
     def __init__(self, map):
         self.name = "Realm"
         self.cities = []
+        self.resources = {resource: 0 for resource in Resource}
     
     def found_city(self, pos):
         city = City(self.map, pos)
         self.cities.append(city)
 
-    
+
         
